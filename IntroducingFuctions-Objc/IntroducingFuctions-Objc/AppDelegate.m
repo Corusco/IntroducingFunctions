746320f16a0c286df26eccef5427641b7dbe5ec1
Justin Huntington <corusco@gmail.com>
2015-04-09 23:06:16 -0600

    Added project code

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -16,14 +16,44 @@
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    // Insert code here...
     
+    //Declare and set new variables called myName and japaneseName to my name
+    NSString *myName = @"Justin";
+    NSString *japaneseName = @"ジャステイン";
+    
+    //call function to pass names printIntroductions
+    countDownTillIntroductions(4);
+    printIntroductions(myName, japaneseName);
     
     return YES;
 }
 
-// Define new functions here...
+//declare function printIntroductions
+void printIntroductions(NSString *name, NSString *funcJapaneseName){
+    
+    //string variables introduction and japanese introduction. Enjoy unicode support.
+    NSString *introduction;
+    NSString *japaneseIntroduction;
+    introduction = [NSString stringWithFormat:@"My name is %@.", name];
+    japaneseIntroduction = [funcJapaneseName stringByAppendingString:@"と申します。よろしくお願いします。"];
+    
+    //log introductions
+    NSLog(@"%@",introduction);
+    NSLog(@"%@", japaneseIntroduction);
+}
+//declare function countDownTillIntroductions
+void countDownTillIntroductions(int numberOfDays){
+    if (numberOfDays == 0) {
+        NSLog(@"The time has come!");
+    }
+    else {
+        NSLog(@"%d days left until introductions", numberOfDays);
+        int oneLessDay = numberOfDays-1;
+        countDownTillIntroductions(oneLessDay);
+    }
+}
+    
+
 
 
 
